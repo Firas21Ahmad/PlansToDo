9dd0f1e85e710e579686fc75d17b1819152e68cd
Firas21Ahmad <you@example.com>
2023-04-21 03:59:07 -0700

    initial

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PlansToDo());
+}
+
+class PlansToDo extends StatelessWidget {
+  const PlansToDo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
